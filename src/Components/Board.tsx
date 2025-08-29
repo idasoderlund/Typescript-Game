51d70cb421cd0472c021d7e39c8be39af8e6edc4
idasoderlund <soderlundida7711@gmail.com>
2025-08-29 15:55:37 +0200

    Added props, import and export

---
+++ src/Components/Board.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Cell from "./Cell";
+import { Player } from "./Types";
+
+interface BoardProps {
+  board: (Player | null)[][];
+  handleCellClick: (row: number, col: number) => void;
+}
+
+const Board: React.FC<BoardProps> = ({ board, handleCellClick }) => {
+  return (
+    <div>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ display: "flex" }}>
+          {row.map((cell, colIndex) => (
+            <Cell
+              key={`${rowIndex}-${colIndex}`}
+              value={cell}
+              onClick={() => handleCellClick(rowIndex, colIndex)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
