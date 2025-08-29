668b0165e0756722f5359d4b32994466c270473e
idasoderlund <soderlundida7711@gmail.com>
2025-08-29 15:54:05 +0200

    added props declarations and return

---
+++ src/Components/Settings.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface GameSettings {
+  size: number;
+  winCondition: number;
+}
+
+interface ControlProps {
+  settings: GameSettings;
+  handleSizeChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  startGame: () => void;
+  gameActive: boolean;
+}
+
+const Controls: React.FC<ControlProps> = ({
+  settings,
+  handleSizeChange,
+  startGame,
+  gameActive,
+}) => {
+  return (
+    <div>
+      <label>
+        Storlek på brädet (2-10):{" "}
+        <input
+          type="number"
+          min={2}
+          max={10}
+          value={settings.size}
+          onChange={handleSizeChange}
+          disabled={gameActive}
+        />
+      </label>
+      <button onClick={startGame}> Start over</button>
+    </div>
+  );
+};
+export default Controls;
