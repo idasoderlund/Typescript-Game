cea89a8bf72716c0bfd358906c0b2d4cc4374124
idasoderlund <soderlundida7711@gmail.com>
2025-08-29 15:53:09 +0200

    Added props and if statement

---
+++ src/Components/Status.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Player } from "./Types";
+
+interface StatusProps {
+  winner: Player | null;
+  moveCount: number;
+  size: number;
+  currentPlayer: Player;
+}
+
+const Status: React.FC<StatusProps> = ({
+  winner,
+  moveCount,
+  size,
+  currentPlayer,
+}) => {
+  if (winner) {
+    return <h2> Winner: {winner}</h2>;
+  }
+  if (moveCount === size * size) {
+    return <h2>Oavgjord match!</h2>;
+  }
+  return <h2> Nu spelar: {currentPlayer}</h2>;
+};
+
+export default Status;
